5df41c9ad7a93e67a19232ff6f3cc7bdd9d4133e
pTimYu <timuuuu7@outlook.com>
2026-02-28 16:38:52 -0500

    Add binary and byte calculations

--- Radio/radio_packet_digits_cal.py
+++ Radio/radio_packet_digits_cal.py
@@ -26,8 +26,13 @@ i = 0
 for index, num in enumerate(digits):
     print(f"{i}-{i + num - 1}: {name[index]}")
     i = i + num
-    
+
+bin_number = sum(digits) * np.log2(10)
+byte_number = bin_number / 8
+print(f"\nBinary number: {bin_number}")
+print(f"Byte number: {byte_number}")
+
 time_on_air_ascii = sum(digits) / 28 ## Estimated time on air time (reduced model)
-time_on_air_bin = sum(digits) * np.log2(10) / 8 / 28
+time_on_air_bin = sum(digits) * byte_number / 28
 print(f"\nTime-on-air by using ASCII: {time_on_air_ascii}s")
 print(f"Time-on-air by using Binary Number: {time_on_air_bin}s")
